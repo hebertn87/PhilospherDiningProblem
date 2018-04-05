bc4e0c7d6353101d7a5a62a2db8a1606f839edf3
Nathan <hebertn87@gmail.com>
2018-04-05 12:27:18 -0600

    Committed final, polished version of the code

--- src/fork/Fork.java
+++ src/fork/Fork.java
@@ -2,16 +2,21 @@ package fork;
 
 public class Fork {
 	
+	//Data members
 	public boolean inUse;
 	public Integer position;
 	
+	//Fork construtor
 	public Fork(Integer _position) {
 		this.position = _position;
 	}
+	
+	//Pick up fork method
 	public synchronized void Hold() {
 		this.inUse = true;
 	}
 	
+	//Drop fork method
 	public synchronized void Drop() {
 		this.inUse = false;
 	}
--- src/philosopher/Philosopher.java
+++ src/philosopher/Philosopher.java
@@ -13,42 +13,56 @@ public class Philosopher implements Runnable {
 
 	private static Logger logger = LogManager.getLogger();
 	
+	//Constructor
 	public Philosopher (Fork _leftFork, Fork _rightFork, Integer _position) {
 		this.rightFork = _rightFork;
 		this.leftFork = _leftFork;
 		this.position = _position;
 	}
 	
+	//Run method
 	public void run(){
-				//Thread loop, includes synchronized
-				while(true) {
-					synchronized(leftFork) {
-						leftFork.Hold();
-						logger.info("Philosopher " + position + " picked up Fork " + leftFork.position);
-						synchronized(rightFork) {
-							rightFork.Hold();
-							logger.info("Philosopher " + position + " picked up Fork " + rightFork.position);
-						
-							//If the current thread holds both their forks, they can eat
-							if(leftFork.inUse && rightFork.inUse) {
-								Eat();			
-							}
+		//Try loop that handles the error level of this run loop
+		try {
+			//Thread loop, includes synchronized
+			while(true) {
+				synchronized(leftFork) {
+					leftFork.Hold();
+					logger.debug("Philosopher " + position + " picked up Fork " + leftFork.position);
+					synchronized(rightFork) {
+						rightFork.Hold();
+						logger.debug("Philosopher " + position + " picked up Fork " + rightFork.position);
+					
+						//If the current thread holds both their forks, they can eat
+						if(leftFork.inUse && rightFork.inUse) {
+							Eat();			
 						}
+						rightFork.Drop();
+						logger.debug("Philosopher " + position + " has dropped fork " + rightFork.position);
 					}
-					Think(); //They think after or as they eat
+					leftFork.Drop();
+					logger.debug("Philosopher " + position + " has dropped fork " + leftFork.position);
 				}
+				Think(); //They think after or as they eat
+			}
+		}catch(Exception e) {
+			logger.error(e);
+		}
 	}
 	
+	//Method to sleep
 	public void Think() {
 		try {
-			logger.info("Philosopher " + position + " is thinking...");
+			logger.trace("Philosopher " + position + " is waiting...");
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			logger.warn("Philosopher " + position + " was interrupted.");
+			logger.warn(e);
 		}
 	}
 	
+	//Method to eat
 	public void Eat() {
 		logger.info("Philosopher " + position + " is eating.");
 	}
