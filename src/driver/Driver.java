435e1f685682032eeecefd5b1b0d8d6841e343ba
Nate Hebert <hebertn87@gmail.com>
2018-04-05 09:21:40 -0600

    Pushed all files up

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,16 +1,34 @@
 package driver;
 
 import fork.Fork;
+import philosopher.Philosopher;
 
 public class Driver {
 	public static void main(String[] args) {
 		
-		Fork[] forks;
+		//Create an array of forks and an array of phils
+		Fork[] forks = new Fork[5];
+		Philosopher[] thinker = new Philosopher[5];
 		
-		
-		
+		//Initialize Fork		
 		for(int i = 0; i < forks.length; i++) {
+			forks[i] = new Fork(i);
+		}
+		
+		
+		//This is currently how my philosopher are instantiated in the loop
+		//thinker 0 = forks[0], forks[1]);
+		//thinker 1 = forks[1], forks[2]);
+		//thinker 2 = forks[2], forks[3]);
+		//thinker 3 = forks[3], forks[4]);
+		//thinker 4 = forks[4], forks[0]);
+		for(int i = 0; i < thinker.length; i++) {
+			Fork leftFork = forks[i];
+			Fork rightFork = forks[(i + 1) % forks.length];
 			
+			thinker[i] = new Philosopher(leftFork, rightFork, i);
+			Thread myThread = new Thread(thinker[i]);
+			myThread.start();
 		}
 	}
 }
--- src/philosopher/Philosopher.java
+++ src/philosopher/Philosopher.java
@@ -14,7 +14,28 @@ public class Philosopher implements Runnable {
 		this.position = _position;
 	}
 	
-	public void run() {
-				
+	public void run(){
+				while(true) {
+					synchronized(leftFork) {
+						leftFork.Hold();
+						System.out.println("Phil " + position + " picked up leftFork " + leftFork.position);
+						synchronized(rightFork) {
+							rightFork.Hold();
+							System.out.println("Phil " + position + " picked up rightFork " + rightFork.position);
+						}
+					}
+					//The think as they eat and before/after they eat
+					Think();
+				}
+	}
+	
+	public void Think() {
+		try {
+			System.out.println("Thinker is thinking...");
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
